f2af19372d40700d7d19f74dec8ca8620ed581ed
Joan Marín <36265613+X0KA@users.noreply.github.com>
2018-10-07 22:29:44 +0200

    Revert "not printing"
    This reverts commit 5214ea3ef6036ce191fc5291f389b72c8a71d51f.

--- Dev_class6_handout/Motor2D/j1Scene.cpp
+++ Dev_class6_handout/Motor2D/j1Scene.cpp
@@ -30,10 +30,9 @@ bool j1Scene::Awake()
 // Called before the first frame
 bool j1Scene::Start()
 {
-	
-	App->map->Load("Aegis_Map.tmx");
 	//App->map->Load("hello2.tmx");
-	//App->map->Load("iso.tmx");
+	App->map->Load("iso.tmx");
+	
 	return true;
 }
 
